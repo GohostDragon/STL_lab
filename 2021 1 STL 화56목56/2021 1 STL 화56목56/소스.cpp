6eb43ba868f26c29e10db72a14c6580d55e4a5cb
MecaDragon <kvg753@gmail.com>
2021-03-25 15:20:01 +0900

    Array 컨테이너 직접 만듦(4주차 2일)

--- 2021 1 STL 화56목56/2021 1 STL 화56목56/소스.cpp
+++ 2021 1 STL 화56목56/2021 1 STL 화56목56/소스.cpp
@@ -4,27 +4,65 @@
 // 2021. 1학기 STL 화56 목56 - 3월 25일 목요일				(4주 2일)
 //
 // 컨테이너는 다른 객체를 저장하는 객체이다.
+// Sequence Containers
+//			- array
+//			- vector
+//			- deque
+//			- forward_list
+//			- list
+//
+// 관찰 class에 이동가능 코딩 - sort 함수로 확인
 //----------------------------------------------------------------------------------
 
 #include <iostream>
-#include <vector>
-#include <string>
-#include <iterator>
-#include <algorithm>
-#include <fstream>
 #include "save.h"
 #include "String.h"
 using namespace std;
 
+// [실습] main이 실행될 수 있게 Array를 프로그램하라
+
+template <typename T, int N>
+class Array {
+	T data[N]{ 0 };
+public:
+	Array() {
+	}
+
+	size_t size() const {
+		return N;
+	}
+
+	T operator[](int idx) const {
+		return data[idx];
+	}
+
+	T& operator[](int idx) {
+		return data[idx];
+	}
+
+	T* begin() {
+		return data;
+	}
+
+	T* end() {
+		return data + N;
+	}
+};
+
 int main()
 {
-	vector<vector <int> > v{ {1,2,3}, {4,5,6,7,8}, {100,200,300} };
+	Array<String, 10> a;
+	
+	for (int i = 0; i < a.size(); ++i)
+		a[i] = i * i;
 
-	for (auto i = v.begin(); i != v.end(); ++i) {
-		auto t = *i;
-		for (auto ti = t.begin(); ti != t.end(); ++ti)
-			cout << *ti << " ";
-		cout << endl;
-	}
+	for (int i = 0; i < a.size(); ++i)
+		cout << a[i] << endl;
+	
+	cout << "반복자로 출력" << endl;
+
+	for (auto i = a.begin(); i != a.end(); ++i)
+		cout << *i << endl;
+
 	save("소스.cpp");
 }
